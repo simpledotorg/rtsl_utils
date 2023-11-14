9b88205987825fc45b4c9f9aee8578eb504801c8
Arnaud DEMARCQ <ademarcq@RTSL-N7VH6K2VP9.local>
2023-11-14 18:14:07 +0100

    Adds asynch support

---
+++ rtsl_util/CommonUtils/OpenmetricsUtils/src/main/java/org/rtsl/openmetrics/utils/asynch/FileMetricConsumer.java
@@ -0,0 +1,39 @@
+package org.rtsl.openmetrics.utils.asynch;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.rtsl.openmetrics.utils.Metric;
+import org.rtsl.openmetrics.utils.MetricProvider;
+
+public class FileMetricConsumer implements Runnable {
+
+    private File metricFile;
+    private MetricProvider<Number> metricProvider;
+
+    public void setMetricFile(File metricFile) {
+        this.metricFile = metricFile;
+    }
+
+    public void setMetricFile(String metricFileName) {
+        this.metricFile = new File(metricFileName);
+    }
+
+    public void setMetricProvider(MetricProvider metricProvider) {
+        this.metricProvider = metricProvider;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(metricFile))) {
+            for (Metric currentMetric : metricProvider.getMetrics()) {
+                writer.write(currentMetric.getAsString());
+                writer.write("\n");
+            }
+        } catch (Exception e) {
+            // TODO : log
+        }
+    }
+
+}
