e16085de009989010b4910d41ed3efa575edc8d8
Arnaud DEMARCQ <ademarcq@RTSL-N7VH6K2VP9.local>
2024-02-02 12:12:36 +0100

    monitoring time now follows conventions

--- rtsl_util/CommonUtils/OpenmetricsUtils/src/main/java/org/rtsl/openmetrics/utils/wrappers/SelfMonitoringMetricProviderWrapper.java
+++ rtsl_util/CommonUtils/OpenmetricsUtils/src/main/java/org/rtsl/openmetrics/utils/wrappers/SelfMonitoringMetricProviderWrapper.java
@@ -53,7 +53,6 @@ public final class SelfMonitoringMetricProviderWrapper implements IMetricProvide
 
         returnList.add(new StandardMetric(metric_prefix + "_generation_time", t1, "monitoring_source", metricProviderName)); // horrible in term of perfs. Should be improved at some point
         returnList.add(new StandardMetric(metric_prefix + "_duration_seconds", durationInSecond, "monitoring_source", metricProviderName)); // horrible in term of perfs. Should be improved at some point
-        returnList.add(new StandardMetric(metric_prefix + "_duration_milliseconds", t2 - t1, "monitoring_source", metricProviderName)); // horrible in term of perfs. Should be improved at some point
         returnList.add(new StandardMetric(metric_prefix + "_metrics_count", wrappedList.size(), "monitoring_source", metricProviderName)); // horrible in term of perfs. Should be improved at some point
         returnList.add(new StandardMetric(metric_prefix + "_error_status", error_count, "monitoring_source", metricProviderName)); // horrible in term of perfs. Should be improved at some point
 
