ab9aa7ec9bc70ddb710837635701afe03cd7ca5c
Arnaud DEMARCQ <ademarcq@RTSL-N7VH6K2VP9.local>
2024-02-05 11:21:54 +0100

    cleaning

--- rtsl_util/CommonUtils/ConfigUtils/src/main/java/org/rtsl/config/dynamic/json/JsonKeyFinder.java
+++ rtsl_util/CommonUtils/ConfigUtils/src/main/java/org/rtsl/config/dynamic/json/JsonKeyFinder.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
