bc5d81a7adb76fea256ada803cac89a5ca12f226
Priyanga P Kini <priyangapkini99@gmail.com>
2024-07-03 18:06:06 +0530

    Add hook skeleton to delete all the test org unit

---
+++ rtsl_util/CommonUtils/Dhis2CucumberTestTool/src/main/java/org/rtsl/dhis2/cucumber/definitions/Hooks.java
@@ -0,0 +1,16 @@
+package org.rtsl.dhis2.cucumber.definitions;
+
+import io.cucumber.java.AfterAll;
+import org.rtsl.dhis2.cucumber.Factories.OrganisationUnit;
+
+public class Hooks {
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Delete all the test org units along with it's dependencies after all scenarios");
+        try {
+            new OrganisationUnit().deleteAll();
+        } catch (Exception e) {
+//            TODO handle exception
+        }
+    }
+}
