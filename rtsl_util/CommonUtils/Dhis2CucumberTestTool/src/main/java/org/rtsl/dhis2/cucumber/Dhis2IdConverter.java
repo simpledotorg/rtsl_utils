04f3623b17a557aa0ed6a56e4e1b762b3d990d03
Priyanga P Kini <priyangapkini99@gmail.com>
2024-07-22 11:32:21 +0530

    Generalise metadata Id convertion in dhis2

--- rtsl_util/CommonUtils/Dhis2CucumberTestTool/src/main/java/org/rtsl/dhis2/cucumber/Dhis2IdConverter.java
+++ rtsl_util/CommonUtils/Dhis2CucumberTestTool/src/main/java/org/rtsl/dhis2/cucumber/Dhis2IdConverter.java
@@ -12,17 +12,15 @@ public final class Dhis2IdConverter {
     private static final Logger LOGGER = LoggerFactory.getLogger(Dhis2IdConverter.class);
     private static final String GET_TEI_ATTRIBUTES_URL = "api/trackedEntityAttributes?paging=false&fields=shortName,name,id";
     private static final String GET_PROGRAM_INDICATOR_URL = "api/programIndicators?paging=false&fields=shortName,name,id";
+    private static final String GET_JOB_CONFIGURATION_URL = "api/jobConfigurations?fields=name,id";
+
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final Dhis2HttpClient dhis2HttpClient;
 
-    private final Map<String, String> teiAttributeIdFromShortName = new HashMap<>();
-    private final Map<String, String> teiAttributeIdFromName = new HashMap<>();
-    private final Map<String, String> teiAttributeIdFromId = new HashMap<>();
-
-    private final Map<String, String> programIndicatorIdFromShortName = new HashMap<>();
-    private final Map<String, String> programIndicatorIdFromName = new HashMap<>();
-    private final Map<String, String> programIndicatorIdFromId = new HashMap<>();
+    private final Map<String, String> idFromShortName = new HashMap<>();
+    private final Map<String, String> idFromName = new HashMap<>();
+    private final Map<String, String> idFromId = new HashMap<>();
 
     public Dhis2IdConverter(Dhis2HttpClient dhis2HttpClient) throws Exception {
         this.dhis2HttpClient = dhis2HttpClient;
@@ -31,6 +29,7 @@ public final class Dhis2IdConverter {
     private void getMetadata()throws Exception {
         getTeiAttributes();
         getProgramIndicators();
+        getJobConfigurations();
     }
 
     private void getTeiAttributes() throws Exception {
@@ -43,9 +42,9 @@ public final class Dhis2IdConverter {
                 String currentName = jsonNode.get("name").asText();
                 String currentShortName = jsonNode.get("shortName").asText();
                 LOGGER.info("creating helper for trackedEntityAttribute {}:{}:{} ", currentId, currentName, currentShortName);
-                teiAttributeIdFromShortName.put(currentShortName, currentId);
-                teiAttributeIdFromName.put(currentName, currentId);
-                teiAttributeIdFromId.put(currentId, currentId);
+                idFromShortName.put(currentShortName, currentId);
+                idFromName.put(currentName, currentId);
+                idFromId.put(currentId, currentId);
 
             }
         }
@@ -61,63 +60,50 @@ public final class Dhis2IdConverter {
                 String currentName = jsonNode.get("name").asText();
                 String currentShortName = jsonNode.get("shortName").asText();
                 LOGGER.info("creating helper for trackedEntityAttribute {}:{}:{} ", currentId, currentName, currentShortName);
-                programIndicatorIdFromShortName.put(currentShortName, currentId);
-                programIndicatorIdFromName.put(currentName, currentId);
-                programIndicatorIdFromId.put(currentId, currentId);
+                idFromShortName.put(currentShortName, currentId);
+                idFromName.put(currentName, currentId);
+                idFromId.put(currentId, currentId);
+            }
+        }
+    }
+    private void getJobConfigurations() throws Exception {
+        String response = dhis2HttpClient.doGet(GET_JOB_CONFIGURATION_URL);
+        JsonNode arrayNode = objectMapper.readTree(response).get("jobConfigurations");
 
+        if (arrayNode.isArray()) {
+            for (JsonNode jsonNode : arrayNode) {
+                String currentId = jsonNode.get("id").asText();
+                String currentName = jsonNode.get("name").asText();
+                LOGGER.info("creating helper for trackedEntityAttribute {}:{} ", currentId, currentName);
+                idFromName.put(currentName, currentId);
+                idFromId.put(currentId, currentId);
             }
         }
     }
 
-    public String getTeiAttributeId(String candidateString) {
-        if (teiAttributeIdFromId.get(candidateString) != null) {
-            LOGGER.debug("String <{}> is already a trackedEntityAttribute id. Using it as it is.", candidateString);
+    public String getMetadataId(String candidateString) {
+        if (idFromId.get(candidateString) != null) {
+            LOGGER.debug("String <{}> is already a metadata id. Using it as it is.", candidateString);
             return candidateString;
         }
-        String returnId = teiAttributeIdFromName.get(candidateString);
+        String returnId = idFromName.get(candidateString);
         if (returnId != null) {
-            LOGGER.debug("String <{}> is the trackedEntityAttribute name for id <{}>", candidateString, returnId);
+            LOGGER.debug("String <{}> is the metadata name for id <{}>", candidateString, returnId);
             return returnId;
         }
-        returnId = teiAttributeIdFromShortName.get(candidateString);
+        returnId = idFromShortName.get(candidateString);
         if (returnId != null) {
-            LOGGER.debug("String <{}> is the trackedEntityAttribute shortName for id <{}>", candidateString, returnId);
+            LOGGER.debug("String <{}> is the metadata shortName for id <{}>", candidateString, returnId);
             return returnId;
         }
-        LOGGER.debug("String <{}> is not known as either an Id, Name or ShortName for trackedEntityAttribute. This is likely going to create a problem", candidateString);
+        LOGGER.debug("String <{}> is not known as either an Id, Name or ShortName for metadata. This is likely going to create a problem", candidateString);
         return candidateString;
     }
 
-    public String getProgramIndicatorId(String candidateString) {
-        if (programIndicatorIdFromId.get(candidateString) != null) {
-            LOGGER.debug("String <{}> is already a trackedEntityAttribute id. Using it as it is.", candidateString);
-            return candidateString;
-        }
-        String returnId = programIndicatorIdFromName.get(candidateString);
-        if (returnId != null) {
-            LOGGER.debug("String <{}> is the trackedEntityAttribute name for id <{}>", candidateString, returnId);
-            return returnId;
-        }
-        returnId = programIndicatorIdFromShortName.get(candidateString);
-        if (returnId != null) {
-            LOGGER.debug("String <{}> is the trackedEntityAttribute shortName for id <{}>", candidateString, returnId);
-            return returnId;
-        }
-        LOGGER.debug("String <{}> is not known as either an Id, Name or ShortName for trackedEntityAttribute. This is likely going to create a problem", candidateString);
-        return candidateString;
-    }
-
-    public Map<String, String> convertTeiAttributes(Map<String, String> inputData) {
+    public Map<String, String> convertMetadata(Map<String, String> inputData) {
         Map<String, String> returnMap = new HashMap<>();
         for (String currentKey : inputData.keySet()) {
-            returnMap.put(getTeiAttributeId(currentKey), inputData.get(currentKey));
-        }
-        return returnMap;
-    }
-    public Map<String, String> convertProgramIndicator(Map<String, String> inputData) {
-        Map<String, String> returnMap = new HashMap<>();
-        for (String currentKey : inputData.keySet()) {
-            returnMap.put(getProgramIndicatorId(currentKey), inputData.get(currentKey));
+            returnMap.put(getMetadataId(currentKey), inputData.get(currentKey));
         }
         return returnMap;
     }
