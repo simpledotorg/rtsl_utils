3b0600313d4e76f71e5a5d437a37333b424e85e2
Owajigbanam Ogbuluijah <igbanam@resolvetosavelives.org>
2024-09-17 21:37:07 +0100

    Add Helper Tests

---
+++ rtsl_util/CommonUtils/Dhis2CucumberTestTool/src/test/java/org/rtsl/dhis2/cucumber/HelperTests.java
@@ -0,0 +1,22 @@
+package org.rtsl.dhis2.cucumber;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelperTests {
+    @Test
+    public void testToISODateTimeString() throws Exception {
+        String actual = Helper.toISODateTimeString("2021-01-01");
+
+        Assertions.assertEquals("2021-01-01T00:00:00.000", actual);
+    }
+
+    @Test
+    public void testToDateTime() throws Exception {
+        var actual = Helper.toDateTime("2021-01-01T00:00:00.000");
+
+        Assertions.assertEquals(2021, actual.getYear());
+        Assertions.assertEquals(1, actual.getMonthValue());
+        Assertions.assertEquals(1, actual.getDayOfMonth());
+    }
+}
