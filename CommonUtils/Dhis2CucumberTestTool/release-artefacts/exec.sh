594f2faf433f670b6f497fe2310e12b7d96a2b4d
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2024-10-17 15:07:37 +0100

    runs in shell, not from folder
    For some reason, this setup runs if someone opens the shell and types in
    the commands to run this. If I double-click on the executable script, I
    cannot read the input.properties; but everything works from running the
    shell script.

--- CommonUtils/Dhis2CucumberTestTool/release-artefacts/exec.sh
+++ CommonUtils/Dhis2CucumberTestTool/release-artefacts/exec.sh
@@ -1,26 +1,34 @@
-#!/bin/sh
+#!/bin/bash -l
 
 cd "$(dirname "$0")"
 
 PROPERTIES_FILE="input.properties"
 SCENARIOS=""
 
+if [ ! -f "$PROPERTIES_FILE" ]; then
+  echo "$PROPERTIES_FILE not found!"
+  read -rsp $'Press enter to continue...\n'
+  exit 1
+fi
+
+cat "$PROPERTIES_FILE"
+
 while IFS='=' read -r key value; do
-  if [[ $key == scenarios ]]; then
-    SCENARIOS=$value
+  if [ "$key" = "scenarios" ]; then
+    SCENARIOS=${value%$'\r'}
   fi
 done < "$PROPERTIES_FILE"
 
-echo $SCENARIOS
+echo "$SCENARIOS"
 
-if [[ $SCENARIOS == "" || $SCENARIOS == "<path-to-features[-folder]>" ]]; then
+if [ -z "$SCENARIOS" ] || [ "$SCENARIOS" = "<path-to-features[-folder]>" ]; then
   echo "Ensure that the 'scenarios=' key is set in input.properties.\n"
-  read -rsp $'Press enter to continue...\n'
+  read -rsp $"Press enter to continue..."
   exit 1
-elif [[ -f $SCENARIOS || -d $SCENARIOS ]]; then
+elif [ -f "$SCENARIOS" ] || [ -d "$SCENARIOS" ]; then
   java -Dorg.rtsl.dhis2.testtool.properties=input.properties -Dlogback.configurationFile=log-config.xml -Dfile.ending=UTF8 -jar Dhis2CucumberTestTool-*.jar --object-factory io.cucumber.spring.SpringFactory --plugin html:./test_reports.html --plugin json:./test_reports.json --plugin pretty --glue "org.rtsl.dhis2.cucumber.definitions" --glue "io.cucumber.spring" --threads 1 $SCENARIOS
-  read -rsp $'Press enter to continue...\n'
+  read -rsp $"Press enter to continue..."
   exit 0
 fi
 
-read -rsp $'Press enter to continue...\n'
+read -rsp $"Press enter to continue..."
